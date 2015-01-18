0ba260e3303fe4bf73d33e5beebcc004ea32b02d
Calvin Cheng <linchuan.cheng@gmail.com>
2015-01-18 13:25:29 +0800

    Clearer to reference 'app' as 'js' in watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         }
     },
     watch: {
-        app: {
+        js: {
             files: ["<%= concat.app.src %>"],
             tasks: ["concat"]
         },
