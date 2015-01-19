2d12da279880d7fa8901a52d7be76389ef451d32
Calvin Cheng <linchuan.cheng@gmail.com>
2015-01-19 14:03:21 +0800

    Use browserify and grunt-browserify to load in mercury dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,51 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
   // task configurations
   var config = {
     // meta data from package.json
-    pkg: grunt.file.readJSON("package.json"),
-    banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
-      "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" +
-      "<%= pkg.homepage ? \"* \" + pkg.homepage + \"\\n\" : \"\" %>" +
-      "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.author.name %>;" +
-      " Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %> */\n",
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today(\'yyyy-mm-dd\') %>\n' +
+      '<%= pkg.homepage ? \'* \' + pkg.homepage + \'\\n\' : \'\' %>' +
+      '* Copyright (c) <%= grunt.template.today(\'yyyy\') %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, \'type\').join(\', \') %> */\n',
     // files that our tasks will use
     files: {
       html: {
-        src: "index.html"
+        src: 'index.html'
       },
       js: {
-        src: [
-          "vendor/js/jquery.js",
-          "js/app.js"
-        ]
+        vendor: [
+
+        ],
+        app: {
+          main: 'js/app.js',
+          compiled: 'generated/js/app.min.js'
+        }
       },
       sass: {
         src: [
-            "sass/**/*.{scss,sass}", 
-            "sass/_partials/**/*.{scss,sass}"
+            'sass/**/*.{scss,sass}', 
+            'sass/_partials/**/*.{scss,sass}'
         ]
-      }
+      },
     },
     // our tasks
+    browserify: {
+      app: {
+        files: {
+          'generated/js/app.min.js': ['js/app.js'],
+        }
+      },
+      options: {
+        debug: true
+      }
+    },
     concat: {
       app: {
-        dest: "generated/js/app.min.js",
-        src: "<%= files.js.src %>"
+        dest: 'generated/js/vendor.min.js',
+        src: ['<%= files.js.vendor %>']
       }
     },
     sass: {
@@ -40,43 +53,43 @@ module.exports = function(grunt) {
           options: {
             sourceMap: true
           },
-          dest: "generated/css/styles.css",
-          src: "<%= files.sass.src %>"
+          dest: 'generated/css/styles.css',
+          src: '<%= files.sass.src %>'
         },
         dist: {
           options: {
-            outputStyle: "compressed"
+            outputStyle: 'compressed'
           },
-          dest: "dist/css/styles.css",
-          src: "<%= files.sass.src %>"
+          dest: 'dist/css/styles.css',
+          src: '<%= files.sass.src %>'
         }
     },
     copy: {
       html: {
         files: {
-          "generated/index.html": "<%= files.html.src %>",
-          "dist/index.html": "<%= files.html.src %>"
+          'generated/index.html': '<%= files.html.src %>',
+          'dist/index.html': '<%= files.html.src %>'
         }
       }
     },
     uglify: {
       options: {
-        banner: "<%= banner %>"
+        banner: '<%= banner %>'
       },
       dist: {
-        src: "<%= concat.app.dest %>",
-        dest: "dist/js/app.min.js"
+        src: '<%= concat.app.dest %>',
+        dest: 'dist/js/vendor.min.js'
       }
     },
     server: {
-        base: (process.env.SERVER_BASE || "generated"),
+        base: (process.env.SERVER_BASE || 'generated'),
         web: {
             port: 8000
         }
     },
     open: {
         dev: {
-            path: "http://localhost:<%= server.web.port %>"
+            path: 'http://localhost:<%= server.web.port %>'
         }
     },
     watch: {
@@ -84,21 +97,25 @@ module.exports = function(grunt) {
             livereload: true
         },
         html: {
-            files: ["<%= files.html.src %>"],
-            tasks: ["copy"]
+            files: ['<%= files.html.src %>'],
+            tasks: ['copy']
         },
         js: {
-            files: ["<%= files.js.src %>"],
-            tasks: ["concat"]
+            files: ['<%= files.js.vendor %>'],
+            tasks: ['concat']
+        },
+        app: {
+            files: ['<%= files.js.app.main =>'],
+            tasks: ['browserify', 'concat']
         },
         sass: {
-            files: ["<%= files.sass.src %>"],
-            tasks: ["sass:dev"]
+            files: ['<%= files.sass.src %>'],
+            tasks: ['sass:dev']
         }
     },
-    // one-off task "grunt clean", which clears our generated files and build files.
+    // one-off task 'grunt clean', which clears our generated files and build files.
     clean: {
-        workspaces: ["dist", "generated"]
+        workspaces: ['dist', 'generated']
     }
   };
 
@@ -106,14 +123,14 @@ module.exports = function(grunt) {
   grunt.initConfig(config);
 
   // loading local tasks
-  grunt.loadTasks("tasks");
+  grunt.loadTasks('tasks');
 
   // loading external tasks (aka: plugins)
-  require("matchdep").filterAll("grunt-*").forEach(grunt.loadNpmTasks);
+  require('matchdep').filterAll('grunt-*').forEach(grunt.loadNpmTasks);
 
   // creating workflows
-  grunt.registerTask("default", ["copy", "sass:dev", "concat", "server", "open", "watch"]);
-  grunt.registerTask("build", ["copy", "sass:dist", "concat", "uglify"]);
-  grunt.registerTask("prodsim", ["build", "server", "open", "watch"]);
+  grunt.registerTask('default', ['copy', 'sass:dev', 'browserify', 'concat', 'server', 'open', 'watch']);
+  grunt.registerTask('build', ['copy', 'sass:dist', 'browserify', 'concat', 'uglify']);
+  grunt.registerTask('prodsim', ['build', 'server', 'open', 'watch']);
 
 };
--- tasks/server.js
+++ tasks/server.js
@@ -1,19 +1,19 @@
 module.exports = function(grunt) {
-    "use strict";
-    var express = require("express");
-    var errorhandler = require("errorhandler");
-    var compression = require("compression");
-    grunt.registerTask("server", "static file development server", function() {
+    'use strict';
+    var express = require('express');
+    var errorhandler = require('errorhandler');
+    var compression = require('compression');
+    grunt.registerTask('server', 'static file development server', function() {
       var app, webPort, webRoot;
-      webPort = grunt.config.get("server.web.port") || 8000;
-      webRoot = grunt.config.get("server.base") || "generated";
+      webPort = grunt.config.get('server.web.port') || 8000;
+      webRoot = grunt.config.get('server.base') || 'generated';
       app = express();
       app.use(compression());
-      app.use(express.static("" + (process.cwd()) + "/" + webRoot));
+      app.use(express.static('' + (process.cwd()) + '/' + webRoot));
       app.use(errorhandler());
       app.listen(webPort);
 
-      grunt.log.writeln("Starting express web server in \"" + webRoot + "\" on port " + webPort);
+      grunt.log.writeln('Starting express web server in \'' + webRoot + '\' on port ' + webPort);
 
       return app;
     });
